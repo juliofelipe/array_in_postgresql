88af07fe2f1daa6e9cc501acee6688119c1c9825
Julio Felipe <julio.felipe@casaldev.com.br>
2021-02-24 10:05:34 -0300

    Update array.sql

--- array.sql
+++ array.sql
@@ -1,3 +1,4 @@
+--CRIA A TABELA COM ARRAY
 CREATE TABLE reajuste_test (
 	id serial PRIMARY KEY,
 	ano_reajuste VARCHAR (100),
@@ -6,11 +7,12 @@ CREATE TABLE reajuste_test (
 	reajustes FLOAT []
 );
 
+--INSERE OS DADOS
 INSERT INTO reajuste_test (ano_reajuste, ano, mes, reajustes)
 VALUES('Ano 1', '{2018, 2018, 2018}','{9, 10, 11}','{8826.85, 18830.60, 8826.85 }'),
       ('Ano 2', '{2018, 2018, 2018}','{9, 10, 11}', '{15440.43, 32939.57, 15440.43 }');
      
-     
+--TRAZ TODOS OS VALORES    
 select ano_reajuste, unnest(ano), unnest(mes), unnest(reajustes)
 from reajuste_test rt 
 
@@ -20,5 +22,5 @@ from reajuste_test rt
 where mes[1] = 9
 group by  ano[1], mes[1]
 
-
+--DELETA A TABELA
 delete from reajuste_test 
